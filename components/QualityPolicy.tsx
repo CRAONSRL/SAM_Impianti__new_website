f0e4d74e939314c582500e39489ac6a161c440ec
Davide Espertini @Satispay <davide.espertini@satispay.com>
2025-10-29 16:19:37 +0100

    little fixes

--- components/QualityPolicy.tsx
+++ components/QualityPolicy.tsx
@@ -1,20 +1,20 @@
-import bottomBanner from "@/app/images/chi-siamo/bottom-banner.jpeg";
+import bottomBanner from "@/app/images/chi-siamo/bottom-banner-new.jpg";
 import texture from "@/app/images/chi-siamo/texture-logo-bianco.png";
 import donwload from "@/app/images/chi-siamo/download.png";
 import Image from "next/image";
 import Link from "next/link";
 
-
+//linear-gradient(to right, rgba(0, 141, 170, 0.66), rgba(0, 141, 170, 0.66), rgba(219, 220, 55, 0.66)),
 const QualityPolicy = () => {
   return (
     <section className="w-full flex flex-col lg:flex-row gap-16 lg:gap-20 px-8 py-8 lg:py-20 relative h-[22rem]"
     style={{
-      backgroundImage: `linear-gradient(to right, rgba(0, 141, 170, 0.66), rgba(0, 141, 170, 0.66), rgba(219, 220, 55, 0.66)),url(${bottomBanner.src})`,
+      backgroundImage: `url(${bottomBanner.src})`,
       backgroundSize: 'cover',
-      backgroundPosition: 'center 20%',
+      backgroundPosition: 'center center',
       backgroundRepeat: 'no-repeat'
     }}>
-      <Image src={texture} alt="Texture" width={1000} height={1000} className="absolute top-0 right-0 w-[50%] h-full " />
+      {/* <Image src={texture} alt="Texture" width={1000} height={1000} className="absolute top-0 right-0 w-[50%] h-full " /> */}
       <div className="flex lg:flex-row w-full flex-col max-w-7xl mx-auto gap-10 lg:gap-14 items-left justify-start text-left lg:text-left lg:items-start">
         <div className="flex flex-row gap-10 w-1/3 items-center absolute justify-around">
           <div className="w-1/2 flex items-center justify-center h-full">
